93b30ae610e4c096e93fe0e95c9b9513f9e74bf4
xin <102291cindy@gmail.com>
2024-08-13 11:01:13 +0800

    修改user資料庫

--- create_sql.sql
+++ create_sql.sql
@@ -14,8 +14,8 @@ CREATE TABLE IF NOT EXISTS user (
 -- 為了提高查詢效率，我們可以在 email 列上添加索引
 CREATE INDEX idx_user_email ON user(email);
 
-INSERT INTO user (email, password, is_first_login, is_admin) VALUES
-('d1047274@o365.fcu.edu.tw','scrypt:32768:8:1$2LQrTDpcEQRSO4D7$d819980a6bf666111958a96d4e0c2618436f7920ff68ba1a42e8a13dc1a9daf0e4399242077a45a3a364483810e2543dacf9ede662d4ad08e67feefcd0931181', FALSE,FALSE);
+INSERT INTO user (name,email, password, is_first_login, is_admin) VALUES
+("仙女",'d1047274@o365.fcu.edu.tw','scrypt:32768:8:1$2LQrTDpcEQRSO4D7$d819980a6bf666111958a96d4e0c2618436f7920ff68ba1a42e8a13dc1a9daf0e4399242077a45a3a364483810e2543dacf9ede662d4ad08e67feefcd0931181', FALSE,FALSE);
 
 
 
